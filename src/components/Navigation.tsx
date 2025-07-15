6d8c72341ea95bfad0fa66cd5b71e26205552fae
sonechas <73230844+sonechas@users.noreply.github.com>
2025-07-15 15:53:21 +0100

    Make logo bigger in navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,7 @@
+@@ .. @@
+            <img 
+              src="/UKProjLogo.png" 
+              alt="UK Project Logo" 
+-              className="h-12 w-auto object-contain"
++              className="h-16 w-auto object-contain"
+            />
